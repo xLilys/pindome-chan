ac4fd63ab197e88adb21b29b8946b2be4d9bd08c
agchan <yuuki617phone@gmail.com>
2023-09-17 01:00:22 +0900

    📌 de pindome

--- src/pindome_func.ts
+++ src/pindome_func.ts
@@ -1,10 +1,15 @@
-import { User, MessageReaction, PartialMessageReaction, PartialUser, ReactionCollector } from 'discord.js'
+import { User, MessageReaction, PartialMessageReaction, PartialUser, ReactionCollector, ReactionEmoji } from 'discord.js'
 
 export function pindome(reaction:MessageReaction|PartialMessageReaction,user:User|PartialUser):void{
     //BOTは無視
     if(user.bot == true)return
 
-    console.log(reaction.emoji.name)
+    //ピン止めが可能であれば実行
+    if(reaction.emoji.name == "📌"){
+        if(reaction.message.pinnable){
+            reaction.message.pin()
+        }
+    }
 
     return
 }
