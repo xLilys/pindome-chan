2db4e00ec582196351713afcf74991041413ee21
agchan <yuuki617phone@gmail.com>
2023-09-17 01:34:24 +0900

    switch de bunki suru

--- src/pindome_func.ts
+++ src/pindome_func.ts
@@ -1,31 +1,36 @@
 import { User, MessageReaction, PartialMessageReaction, PartialUser, Message, MessagePayload } from 'discord.js'
 
-export function pindome(reaction:MessageReaction|PartialMessageReaction,user:User|PartialUser):void{
+export function pindome(reaction: MessageReaction | PartialMessageReaction, user: User | PartialUser): void {
     //BOTは無視
-    if(user.bot == true)return
+    if (user.bot == true) return
 
-    //ピン止めが可能であれば実行
-    if(reaction.emoji.name == "📌"){
-        if(reaction.message.pinnable){
-            reaction.message.pin()
-            let sendmsg = "@" + user.toString() + "\nメッセージをピン留めしましたっ！"
-            reaction.message.channel.send(sendmsg)
-        }else{
-            let sendmsg = "@" + user.toString() + "\nメッセージをピン留めできなかったよ"
-            reaction.message.channel.send(sendmsg)
+    const pin_emoji = "📌"
+
+    switch (reaction.emoji.name) {
+        case pin_emoji: {
+
+            //ピン止めが可能であれば実行
+            if (reaction.message.pinnable) {
+                reaction.message.pin()
+                let sendmsg = "@" + user.toString() + "\nメッセージをピン留めしましたっ！"
+                reaction.message.channel.send(sendmsg)
+            } else {
+                let sendmsg = "@" + user.toString() + "\nメッセージをピン留めできなかったよ"
+                reaction.message.channel.send(sendmsg)
+            }
         }
     }
 
     return
 }
 
-export function erase_pin_automsg(me:User|null,message:Message):void{
-    if(me != null){
-        if(message.system){
-            if(message.author.id == me.id){
-                if(message.deletable){
+export function erase_pin_automsg(me: User | null, message: Message): void {
+    if (me != null) {
+        if (message.system) {
+            if (message.author.id == me.id) {
+                if (message.deletable) {
                     message.delete()
-                }else{
+                } else {
                 }
             }
         }
