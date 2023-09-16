5cc3e9d973d4544cca4e16898415634b53888227
agchan <yuuki617phone@gmail.com>
2023-09-17 00:07:35 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { GatewayIntentBits, Client, Partials, Message } from 'discord.js'
+import dotenv from 'dotenv'
+
+//.envファイルを読み込む
+dotenv.config()
+
+//Botで使うGetwayIntents、partials
+const client = new Client({
+    intents: [
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessageReactions
+    ],
+    partials: [Partials.Message, Partials.Channel],
+})
+
+client.once('ready', () => {
+    console.log('Ready!')
+    if (client.user) {
+        console.log(client.user.tag)
+    }
+})
+
+//ボット作成時のトークンでDiscordと接続
+client.login(process.env.TOKEN)
